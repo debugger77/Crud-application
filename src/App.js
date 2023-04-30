eb87436391046c8bf857ae98fceec9a725c98b32
debugger77 <devhr78@gmail.com>
2023-04-30 14:59:51 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+//components
+import NavBar from './components/NavBar';
+import CodeforInterview from './components/CodeforInterview';
+import AddUser from './components/AddUser';
+import AllUsers from './components/AllUsers';
+import EditUser from './components/EditUser';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element = {<CodeforInterview />} />
+        <Route path="/all" element = {<AllUsers />} />
+        <Route path="/add" element = {<AddUser />} />
+        <Route path="/edit/:id" element = {<EditUser />} />
+      </Routes>
+    </BrowserRouter>    
+  );
+}
+
+export default App;
